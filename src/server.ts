318ff411c482b69f99e5b8222e0640c37e20e73b
@mpaez1 <mpaez@ikatechsolutions.com>
2025-03-01 00:03:09 -0500

    add-changes

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://tudominio.com"], // Ajusta según tu frontend
+    origin: ["http://127.0.0.1:5500", "https://tudominio.com"], // Ajusta según tu frontend
     methods: ["GET", "POST"]
   }
 });
